8609878101c2e0bf5bd459d8d3a7e0dc820677b4
RapaxDB <23dimon19@gmal.com>
2023-08-09 22:48:28 +0500

    Com23

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,5 +9,10 @@ public class Main {
         for (int i = 1; i <= 5; i++) {
             System.out.println("i = " + i);
         }
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("i = " + i);
+        }
+        System.out.println("i = https://github.com/RapaxDB/firstProject");
+        System.out.println("i = https://github.com/RapaxDB/firstProject");
     }
 }
